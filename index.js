81ba77ceb1e2cba63dd14ca645cb3c2ae659742e
Kenny Blundell <kenny.blundell@capgemini.com>
2019-04-24 16:27:00 +0100

    Unit 1 pt 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('index.js executing');
+
+var express = require('express');
+var router = require('./routes/hello.js');
+var app = express();
+
+
+
+// whenever i get something of root /, use router as defined
+app.use('/', router);
+
+// basically, here the logic is actually defined within the Router object or whatnot, which can be imported into the main file
+
+
+var port = 3000;
+
+app.listen(port,function(){
+	console.log('Listening on port ' + port);
+});
